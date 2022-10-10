620e11ef534d43acff9d1afad0d38d980ccd1cad
unknown <illquo26@gmail.com>
2022-10-11 01:14:20 +1000

    added chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,32 +20,11 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:yes@gmail.com\">yes@gmail.com</a>")
 }
 
-/* func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		errorHandler(w, r)
-	}
-} */
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		errorHandler(w, r)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(errorHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe("127.0.0.1:3000", router)
+	http.ListenAndServe("127.0.0.1:3000", r)
 }
