18937e269e93bf30b4d45f0896134e3af9efb9e7
Quo <quo@Kranon.localdomain>
2022-10-15 01:40:03 +1000

    added layout page

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func executeTemplate(filepath string) views.Template {
-	tpl, err := views.ParseFS(templates.FS, filepath)
+func executeTemplate(filepath ...string) views.Template {
+	tpl, err := views.ParseFS(templates.FS, filepath...)
 	if err != nil {
 		panic(err)
 	}
@@ -25,9 +25,9 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", controllers.StaticHandler(executeTemplate("home.gohtml")))
-	r.Get("/contact", controllers.StaticHandler(executeTemplate("contact.gohtml")))
-	r.Get("/faq", controllers.FAQ(executeTemplate("faq.gohtml")))
+	r.Get("/", controllers.StaticHandler(executeTemplate("layout.gohtml", "home.gohtml")))
+	r.Get("/contact", controllers.StaticHandler(executeTemplate("layout.gohtml", "contact.gohtml")))
+	//r.Get("/faq", controllers.FAQ(executeTemplate("layout.gohtml", "faq.gohtml")))
 	r.NotFound(errorHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe("127.0.0.1:3000", r)
